afdf7b1e31f7b95b286e1d05f77dbc6cf23f2473
Anthony Rodriguez <anthonyr38@proton.me>
2023-05-24 14:00:51 -0400

    Static Exercise 1

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempConverter.FahrenheitToCelsius(68); // 20
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20); //68
+
+            Console.WriteLine("Display temperature in celsius and fahrenheit:\n");
+            Console.WriteLine($"celsius is: {celsius}\n");
+            Console.WriteLine($"fahrenheit is: {fahrenheit}");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit) 
+        {
+            double resultToCelsius = (fahrenheit - 32) / 1.8;
+            
+            return resultToCelsius;
+        }
+        public static double CelsiusToFahrenheit(double celsius) 
+        {
+            double resultToFahrenheit = (celsius * 9) / 5 + 32;
+
+            return resultToFahrenheit;
+        }
+    }
+}
