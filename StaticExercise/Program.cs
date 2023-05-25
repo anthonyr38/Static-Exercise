51876eac4a84c696c2d5ab4e116658ed33c43d54
Anthony Rodriguez <anthonyr38@proton.me>
2023-05-25 09:28:20 -0400

    Static Exercise 1

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -8,8 +8,8 @@
             var fahrenheit = TempConverter.CelsiusToFahrenheit(22); 
 
             Console.WriteLine("Display temperature in celsius and fahrenheit:\n");
-            Console.WriteLine($"celsius is: {celsius}\n");
-            Console.WriteLine($"fahrenheit is: {fahrenheit}");
+            Console.WriteLine($"74 degrees fahrenheit in celsius is: {celsius} degrees celsius.\n");
+            Console.WriteLine($"22 degrees celsius in fahrenheit is: {fahrenheit} degrees fahrenheit.");
         }
     }
 }
